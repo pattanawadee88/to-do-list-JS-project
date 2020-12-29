66f92de4e294c0edca5198d6438b22ddef9dd783
pattanawadee88 <winyaratp@wit.edu>
2020-12-29 15:17:40 -0500

    add icons and update app.js

--- app.js
+++ app.js
@@ -1,7 +1,29 @@
-var textNew = document.querySelector('input');
-var list = document.getElementsByClassName('list-items-container');
-
-function getTextInput(){
-  document.querySelector(".list-items-container").innerHTML = textNew.value; 
-}
-
+$(document).ready(function(){
+  $("#add-btn").on("click", function(){
+    if($("#input-text").val() !== '')
+    {
+      var checkBox = $("<i class='far fa-check-circle'></i>").click(function(){
+        var x = $(this).parent();
+        x.fadeOut(function(){
+          $(".doneTask").append(x);
+          x.fadeIn();
+        })
+        $(this).remove();
+        $(edit).remove();
+      });
+      var task = $("<div class='task'></div>").text($("#input-text").val());
+      var edit = $("<i class='far fa-edit'></i>").click(function(){
+         
+      });
+      var tash = $("<i class='fas fa-trash-alt'></i>").click(function(){
+        $(task).remove(); 
+      });
+      task.append(checkBox,edit,tash);
+      $(".notDoneTask").append(task);
+      $("#input-text").val("");
+    }else {
+      
+      alert("There is something wrong.");
+    }
+  })
+})
